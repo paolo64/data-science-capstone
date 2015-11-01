65a912216cea8afa2dbda1e6c89c594f612a1108
Paolo Ruscitti <paolo.ruscitti@gmail.com>
2015-11-02 00:00:29 +0100

    added score normalization

--- Python/rankUsers.py
+++ Python/rankUsers.py
@@ -3,6 +3,8 @@ import json
 import sys,os
 from datetime import datetime
 
+import numpy as np
+from sklearn import preprocessing
 from pprint import pprint as pp
 import time
 from staticScoreUser import StaticScoreUser
@@ -54,7 +56,28 @@ class RankUsers:
                 u['score'] = score_dict['score']
                 ret.append(u)
 
-        return ret     
+        return ret
+
+    def score_normalizer(self,data_in):
+        logger.info("starting normalization")
+        scores = np.array([x['score'] for x in data_in])  
+
+        # scaling (minus mean / sd())
+        scores_scaled = preprocessing.scale(scores)
+
+        # normalize min max, range 0,100
+        min_max_scaler = preprocessing.MinMaxScaler(feature_range=(0, 100))
+        scores_minmax = min_max_scaler.fit_transform(scores)
+
+        # enrich final data
+        ret = list()
+        for i,x in enumerate(data_in):
+            x['scores_scaled'] = scores_scaled[i]
+            x['scores_minmax'] = scores_minmax[i]
+            ret.append(x)
+
+        logger.info("end normalization")        
+        return ret    
         
     def main(self):
         logger.info("Start")
@@ -65,13 +88,19 @@ class RankUsers:
         # calc static rank per user
         data_out = self.calcStaticRank()
 
+        # normalize: scale and minmax
+        data_out_normalized = self.score_normalizer(data_out)
+
         # save json
-        self.saveJson(OUTFILE_USR, data_out)
+        self.saveJson(OUTFILE_USR, data_out_normalized)
         
         elapsed = (time.clock() - start)
         
         print "done in %d secs"%int(elapsed)
 
+
+
 if __name__ == "__main__":
     ru = RankUsers()
     ru.main()
+
